94caef67e45ae97818ee7418b6ccdb483fc12e22
Diptendu Barman <b.diptendu@magicminds.io>
2026-09-08 20:08:17 +0530

    Comment out ARI service initialization and shutdown methods for debugging purposes

--- app.py
+++ app.py
@@ -5,12 +5,12 @@ import sys
 from handlers.customer_lookup_handler import process_customer_lookup
 # from handlers.external_media_handler import get_channel_var, process_bridge_end, process_bridge_start
 from services.ami_service import AMIService
-from services.ari_service import ARIService
+# from services.ari_service import ARIService
 from services.logger_service import logger
 
 # Initialize AMI connection
 ami = AMIService()
-ari = ARIService()
+# ari = ARIService()
 
 # High-concurrency worker thread pool
 executor = ThreadPoolExecutor(max_workers=50, thread_name_prefix="ami_worker")
@@ -31,7 +31,7 @@ def shutdown(signum, frame):
     logger.info("Shutdown signal received. Shutting down Server 1 manager...")
     executor.shutdown(wait=False)
     try:
-        ari.stop()
+        # ari.stop()
         ami.disconnect()
     except Exception:
         logger.exception("Error disconnecting AMI")
@@ -44,7 +44,7 @@ if __name__ == "__main__":
 
     try:
         # Initialize Asterisk ARI Service
-        ari.start()
+        # ari.start()
         
         # Initialize Asterisk AMI Service
         ami.start()
